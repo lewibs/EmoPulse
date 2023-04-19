33a2879a1428f6cacf191700bea4574d6594207e
lewibs <benjaminsl2000@gmail.com>
2023-04-19 11:35:45 -0400

    food time

--- src/datareader.py
+++ src/datareader.py
@@ -1,23 +1,33 @@
 import abc
+import files
 
 class DataReader(abc.ABC):
     #FIELDS
     @property
-    @abc.abstractmethod
     def path(self):
-        pass
+        return self._path
+
+    @path.setter
+    def path(self, value):
+        self._path = value
 
     @property
     @abc.abstractmethod
     def name(self):
-        pass
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        self._name = value
     
     #CONSTRUCTOR
     @abc.abstractmethod
     def __init__(self):
+        #do this
+        #self.name = "name of thing"
+        #self.requestFilePath()
         pass
 
     #METHODS
-    @abc.abstractmethod
     def requestFilePath(self):
-        pass
+        self.path = files.makeFileRequester(self.name)()
