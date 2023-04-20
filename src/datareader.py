b94249a46f60ca6b66fe72948458f17acfff40fe
lewibs <benjaminsl2000@gmail.com>
2023-04-19 20:20:53 -0400

    working on reading the csv

--- src/datareader.py
+++ src/datareader.py
@@ -1,5 +1,6 @@
 import files
 import csv
+import datetime
 
 class DataReader():
     #FIELDS
@@ -10,18 +11,25 @@ class DataReader():
     def __init__(self, name):
         self._name = name
         self.requestFilePath()
+        self.readFile()
 
     #METHODS
     def requestFilePath(self):
         self._path = files.makeFileRequester(self._name)()
 
     def readFile(self):
+        data = {}
         # opening the CSV file
-        # with open('Giants.csv', mode ='r')as file:
-        
-        # # reading the CSV file
-        # csvFile = csv.reader(file)
-        
-        # # displaying the contents of the CSV file
-        # for lines in csvFile:
-        #         print(lines)
+        with open(self._path, mode ='r') as file:
+            # reading the CSV file
+            csvFile = csv.reader(file)
+            head = next(csvFile)
+            for line in csvFile:
+                entry = {}
+                for i in range(0, len(head)):
+                    entry[head[i]] = line[i]
+                
+                #get unix start
+                start = datetime.datetime(entry["date"])
+
+            return data
